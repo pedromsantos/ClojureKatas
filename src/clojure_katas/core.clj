31f1e83088af5844fc93cc2bbaf415d8e2190e14
Pedro Santos <github@mariodelrio.com>
2016-07-23 12:04:51 +0100

    Clojure idiomatic formating

--- src/clojure_katas/core.clj
+++ src/clojure_katas/core.clj
@@ -1,11 +1,11 @@
 (ns clojure-katas.core)
 
 (defn fizzBuzz [n] 
-        (let [divisible-by? #(= 0 (rem %2 %1))
-              divisible-by-three? (divisible-by? 3 n)
-              divisible-by-five? (divisible-by? 5 n)]
-        (cond 
-                (and divisible-by-three? divisible-by-five?) "fizzbuzz"
-                divisible-by-three? "fizz"
-                divisible-by-five? "buzz"
-                :else n)))
+  (let [divisible-by? #(= 0 (rem %2 %1))
+    divisible-by-three? (divisible-by? 3 n)
+    divisible-by-five? (divisible-by? 5 n)]
+    (cond 
+      (and divisible-by-three? divisible-by-five?) "fizzbuzz"
+        divisible-by-three? "fizz"
+        divisible-by-five? "buzz"
+        :else n)))
