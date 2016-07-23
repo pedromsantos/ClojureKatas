4f6f4273b142265d06ff3cb5a7e048b001eee13c
Pedro Santos <github@mariodelrio.com>
2016-07-23 01:46:18 +0100

    Create project

--- README.md
+++
@@ -1,2 +0,0 @@
-# ClojureKatas
-Katas in Clojure
---
+++ test/clojure_katas/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-katas.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-katas.core :refer :all]))
+
+(deftest a-test1
+  (testing "Basic tests"
+    ))
