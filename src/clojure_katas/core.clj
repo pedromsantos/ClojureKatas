cfa02a23d6dfc02c1b3df9f97d5c5c9f964edb9a
Pedro Santos <github@mariodelrio.com>
2016-07-23 02:30:16 +0100

    Remove duplication

--- src/clojure_katas/core.clj
+++ src/clojure_katas/core.clj
@@ -1,8 +1,10 @@
 (ns clojure-katas.core)
 
 (defn fizzBuzz [n] 
+        (let [divisible-by-three? (= (rem n 3) 0)
+              divisible-by-five? (= (rem n 5) 0)]
         (cond 
-                (and (= (rem n 3) 0) (= (rem n 5) 0)) "fizzbuzz"
-                (= (rem n 3) 0) "fizz"
-                (= (rem n 5) 0) "buzz"
-                :else n))
+                (and divisible-by-three? divisible-by-five?) "fizzbuzz"
+                divisible-by-three? "fizz"
+                divisible-by-five? "buzz"
+                :else n)))
