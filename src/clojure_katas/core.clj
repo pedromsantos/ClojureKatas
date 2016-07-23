539ee074eb6fc69f3fd8ea852dec845b94a5ee82
Pedro Santos <github@mariodelrio.com>
2016-07-23 12:01:32 +0100

    More terse divisible-by?

--- src/clojure_katas/core.clj
+++ src/clojure_katas/core.clj
@@ -1,7 +1,7 @@
 (ns clojure-katas.core)
 
 (defn fizzBuzz [n] 
-        (let [divisible-by? (fn [divisor n] (= 0 (rem n divisor)))
+        (let [divisible-by? #(= 0 (rem %2 %1))
               divisible-by-three? (divisible-by? 3 n)
               divisible-by-five? (divisible-by? 5 n)]
         (cond 
