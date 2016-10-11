c0388f83841c9f63057d882688c246983cf06330
Pedro Santos <github@mariodelrio.com>
2016-10-11 13:49:45 +0100

    Code formating

--- src/clojure_katas/fizzbuzz.clj
+++ src/clojure_katas/fizzbuzz.clj
@@ -2,9 +2,9 @@
 
 (defn fizzBuzz [n] 
   (let [divisible-by? #(= 0 (rem %2 %1))
-    divisible-by-three? (divisible-by? 3 n)
-    divisible-by-five? (divisible-by? 5 n)
-    divisible-by-three-and-five? (divisible-by? 15 n)]
+        divisible-by-three? (divisible-by? 3 n)
+        divisible-by-five? (divisible-by? 5 n)
+        divisible-by-three-and-five? (divisible-by? 15 n)]
     (cond 
       divisible-by-three-and-five? "fizzbuzz"
       divisible-by-three? "fizz"
